3078327d18f82c7a58dd9b43e14458bcf7addba3
Marc Tucker <marcanthony.tucker@theinformationlab.com>
2025-07-16 15:45:47 +0000

    added limit_data_indev_macro

---
+++ macros/limit_data_in_dev.sql
@@ -0,0 +1,5 @@
+{%- macro limit_data_in_dev(column_name, num_of_days=3) -%}
+{%- if target.name == 'default' -%}
+where {{ column_name }} >= dateadd('day', -{{num_of_days}}, current_timestamp)
+{%- endif -%}
+{%- endmacro -%}
