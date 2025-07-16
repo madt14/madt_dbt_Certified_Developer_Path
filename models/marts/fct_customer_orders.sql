272f63f6a852945570115fbb2e15364f75dd7dcd
Marc Tucker <marcanthony.tucker@theinformationlab.com>
2025-07-16 02:09:52 +0000

    Pull out intermediates queries and simple subqueries into logical CTE groupings, simplified some queries, changed join into window functions, created staging and intermediate models

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -4,100 +4,89 @@ with
 
 customers as (
 
-  select * from {{ source('jaffle_shop', 'customers') }}
+  select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ) ,
 
 orders as (
 
-  select * from {{ source('jaffle_shop', 'orders') }}
+  select * from {{ ref('stg_jaffle_shop__orders') }}
 
-) ,
+),
 
-payments as (
+paid_orders as (
 
-  select * from {{ source('stripe', 'payment') }}
+    select 
+        *
+    FROM {{ ref('int_orders') }}
 
 ) ,
 
 --- Logical CTEs ---
 
-
-
---- Final CTE ---
-
-
-
---- Simple Select Statement ---
-
-
-paid_orders as (
-
-    select 
-        orders.id as order_id,
-        orders.user_id as customer_id,
-        orders.order_date as order_placed_at,
-        orders.status as order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
-        c.first_name as customer_first_name,
-        c.last_name as customer_last_name
-    from orders
-    left join (
-        select 
-            orderid as order_id,
-            max(created) as payment_finalized_date,
-            sum(amount) / 100.0 as total_amount_paid
-        from payments
-        where status <> 'fail'
-        group by 1
-        ) p on orders.id = p.order_id
-left join customers as c on orders.user_id = c.id 
-
-) ,
-
 customer_orders as (
     
     select 
-        c.id as customer_id,
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(orders.id) as number_of_orders
-    from customers as c 
-    left join orders on orders.user_id = c.id 
+        customers.customer_id,
+        min(orders.order_placed_at) as first_order_date,
+        max(orders.order_placed_at) as most_recent_order_date,
+        count(orders.order_id) as number_of_orders
+    from customers
+    left join orders on orders.customer_id = customers.customer_id 
     group by 1
+
+) ,
+
+clv_by_order as (
+
+        (
+        select
+            order_id,
+            sum(total_amount_paid) over (
+                partition by customer_id
+                order by order_id
+            ) as customer_lifetime_value
+        from paid_orders
+    )
+
+) ,
+
+--- Final CTE ---
+
+final as (
+
+    select
+        paid_orders.order_id,
+        paid_orders.customer_id,
+        paid_orders.order_placed_at,
+        paid_orders.order_status,
+        paid_orders.total_amount_paid,
+        paid_orders.payment_finalized_date,
+        customers.customer_first_name,
+        customers.customer_last_name,
+        row_number() over 
+            (
+                order by paid_orders.order_id
+            ) as transaction_seq,
+        row_number() over (
+            partition by paid_orders.customer_id 
+            order by paid_orders.order_id
+        ) as customer_sales_seq,
+        case 
+            when customer_orders.first_order_date = paid_orders.order_placed_at
+            then 'new'
+            else 'return' 
+        end as nvsr,
+        clv_by_order.customer_lifetime_value,
+        customer_orders.first_order_date as fdos
+    from paid_orders
+    left join customer_orders using (customer_id)
+    left outer join clv_by_order on clv_by_order.order_id = paid_orders.order_id
+    left join customers on paid_orders.customer_id = customers.customer_id
+    order by order_id
 
 )
 
-select
-    p.*,
-    row_number() over 
-        (
-            order by p.order_id
-        ) as transaction_seq,
-    row_number() over (
-        partition by customer_id 
-        order by p.order_id
-    ) as customer_sales_seq,
-    case 
-        when c.first_order_date = p.order_placed_at
-        then 'new'
-        else 'return' 
-    end as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-from paid_orders p
-left join customer_orders as c using (customer_id)
-left outer join 
-(
-    select
-        p.order_id,
-        sum(t2.total_amount_paid) as clv_bad
-    from paid_orders p
-    left join paid_orders t2 
-        on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-    group by 1
-    order by p.order_id
-) 
-x on x.order_id = p.order_id
-order by order_id
+--- Simple Select Statement ---
+
+select * from final
