4d4934f1b1f52329bcf4c8018310f7951d449785
Marc Tucker <marcanthony.tucker@theinformationlab.com>
2025-07-16 16:42:34 +0000

    referencing_limit_data_in_dev_macro

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -11,9 +11,12 @@ transformed as (
         id as payment_id,
         orderid as order_id,
         status as payment_status,
+        created as payment_made_on,
         {{cents_to_dollars('amount',0)}} as payment_amount
     from source
 
 )
 
 select * from transformed
+
+{{ limit_data_in_dev('payment_made_on', 10000) }}
