8eb328e8c4ce9778e87c4db30c6217e59bb3b55b
Marc Tucker <marcanthony.tucker@theinformationlab.com>
2025-07-16 19:35:21 +0000

    using_results_from_jinja_dynamically_in_sql_code_and_union_tables_be_prefix

---
+++ models/union_tables_by_prefix_model.sql
@@ -0,0 +1,22 @@
+{%- set tables=['union__part1', 'union__part2'] -%}
+
+
+{%- for table in tables -%}
+    create table if not exists {{ table }} (
+        id int primary key,
+        name varchar
+    );
+
+{% endfor %}
+
+{#
+{{ union_tables_by_prefix(
+
+      database='TIL_PORTFOLIO_PROJECTS',
+      schema='dbt_mtucker', 
+      prefix='union__'
+        
+      )
+      
+}}
+#}
