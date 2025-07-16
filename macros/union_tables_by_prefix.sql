67c76b6e487b0afd0c7f8e071cc7e76e879e44e3
Marc Tucker <marcanthony.tucker@theinformationlab.com>
2025-07-16 20:47:14 +0000

    fixed_union_tables_by_prefix_macro_with_begin/end_block

--- macros/union_tables_by_prefix.sql
+++ macros/union_tables_by_prefix.sql
@@ -1,15 +1,15 @@
 {%- macro union_tables_by_prefix(database, schema, prefix) -%}
 
-  {%- set tables = dbt_utils.get_relations_by_prefix(database=database, schema=schema, prefix=prefix) -%}
+    {%- set tables = dbt_utils.get_relations_by_prefix(database=database, schema=schema, prefix=prefix) -%}
+    
+    {% do log("Found tables: " ~ tables, info=True) %}
 
-  {% for table in tables %}
 
-      {%- if not loop.first -%}
-      union all 
-      {%- endif %}
-        
-      select * from {{ table.database }}.{{ table.schema }}.{{ table.name }}
-      
-  {% endfor -%}
-  
+    {% for table in tables %}
+        {% if not loop.first %}
+            union all
+        {% endif %}
+        select * from {{ table }}
+    {% endfor %}
+
 {%- endmacro -%}
--- models/union_tables_by_prefix_model.sql
+++ models/union_tables_by_prefix_model.sql
@@ -1,9 +1,19 @@
-{{ union_tables_by_prefix(
+{%- set tables=['union__part1', 'union__part2'] -%}
 
-      database='TIL_PORTFOLIO_PROJECTS',
-      schema='dbt_mtucker', 
-      prefix='payment'
-        
-      )
-      
+
+{%- for table in tables -%}
+    create table if not exists {{ table }} (
+        id int primary key,
+        name varchar
+    );
+
+insert into {{ table }} values (1, 'test_{{ table }}');
+
+{% endfor %}
+
+
+{{
+    union_tables_by_prefix(
+        database="TIL_PORTFOLIO_PROJECTS", schema="dbt_mtucker", prefix="union__"
+    )
 }}
