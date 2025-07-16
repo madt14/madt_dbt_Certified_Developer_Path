ead8a3ae4bd86d9dc3897b7657243e54aab322a4
Marc Tucker <marcanthony.tucker@theinformationlab.com>
2025-07-16 14:59:37 +0000

    cents_to_dollars_macro

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -11,7 +11,7 @@ transformed as (
         id as payment_id,
         orderid as order_id,
         status as payment_status,
-        round(amount/100.0, 2) as payment_amount
+        {{cents_to_dollars('amount',0)}} as payment_amount
     from source
 
 )
