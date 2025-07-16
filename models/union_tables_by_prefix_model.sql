3b22d8d85db02396740ff1110df54db8b6e13040
Marc Tucker <marcanthony.tucker@theinformationlab.com>
2025-07-16 19:36:25 +0000

    attempting_to_fix_union_tables_by_prefix

--- models/union_tables_by_prefix_model.sql
+++ models/union_tables_by_prefix_model.sql
@@ -1,22 +1,9 @@
-{%- set tables=['union__part1', 'union__part2'] -%}
-
-
-{%- for table in tables -%}
-    create table if not exists {{ table }} (
-        id int primary key,
-        name varchar
-    );
-
-{% endfor %}
-
-{#
 {{ union_tables_by_prefix(
 
       database='TIL_PORTFOLIO_PROJECTS',
       schema='dbt_mtucker', 
-      prefix='union__'
+      prefix='payment'
         
       )
       
 }}
-#}
